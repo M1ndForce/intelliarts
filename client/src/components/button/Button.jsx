da71d558139fc8e8184fb7a6477925aa0923ddbe
Serhii Durunda <66359796+M1ndForce@users.noreply.github.com>
2021-05-24 00:02:27 +0300

    Delete client directory

--- client/src/components/button/Button.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-const Button = ({text, onclick}) => {
-        return <Button onClick={onclick}>{text} </Button>
-    }
-;
-
-export default Button;
--- client/src/components/input/Input.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-const Input = ({value, name, onChange}) => {
-        return <input type="text" value={value} name={name} onChange={onChange}/>
-    }
-;
-
-export default Input;
