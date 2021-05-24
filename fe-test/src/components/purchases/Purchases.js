731da081435eca5d996a2bf2d5481a069dc128cc
M1ndForce <66359796+M1ndForce@users.noreply.github.com>
2021-05-25 01:05:09 +0300

    fix BE

--- fe-test/src/components/purchases/Purchases.js
+++ fe-test/src/components/purchases/Purchases.js
@@ -4,71 +4,72 @@ import Button from "../../components/button/Button";
 import axios from "axios";
 
 const AddPurchase = () => {
-    axios.defaults.baseURL = "http://localhost:5000";
+  axios.defaults.baseURL = "http://localhost:5000";
 
-    const [name, setName] = useState("");
-    const [count, setCount] = useState("");
-    const [toggle, setToggle] = useState(false);
-    const [error, setError] = useState(null);
-    const [response, setResponse] = useState(null);
+  const [name, setName] = useState("");
+  const [date, setDate] = useState("");
+  const [toggle, setToggle] = useState(false);
+  const [error, setError] = useState(null);
+  const [response, setResponse] = useState(null);
 
-    function inputOnchangeHandler(e) {
-        if (e.target.id === "itemName") {
-            setName(e.target.value);
-        }
-        if (e.target.id === "itemCount") {
-            setCount(e.target.value);
-        }
+  function inputOnchangeHandler(e) {
+    if (e.target.id === "itemNameForPurchase") {
+      setName(e.target.value);
     }
-    function changeToggleToFalse() {
-        setError(null);
-        setResponse(null);
-        return setToggle(false);
+    if (e.target.id === "date") {
+      setDate(e.target.value);
     }
+  }
+  function changeToggleToFalse() {
+    setError(null);
+    setResponse(null);
+    return setToggle(false);
+  }
 
-    const addItemBtnHandler = (e) => {
-        axios
-            .put("/categories", {
-                name: name,
-                count: +count,
-            })
-            .then((res) => {
-                setToggle(true);
-                setName("");
-                setCount("");
-                if (res?.data?.name) {
-                    setResponse(res.data);
-                    setTimeout(changeToggleToFalse, 5000);
-                } else {
-                    setError(res?.data.message);
-                    setTimeout(changeToggleToFalse, 5000);
-                }
-            });
-        e.preventDefault();
-    };
+  const addItemBtnHandler = (e) => {
+    axios
+      .post("/purchases", {
+        name: name,
+        date: date,
+      })
+      .then((res) => {
+        console.log(res.data);
+        setToggle(true);
+        setName("");
+        setDate("");
+        if (res?.data?.name) {
+          setResponse(res.data);
+          setTimeout(changeToggleToFalse, 5000);
+        } else {
+          setError(res?.data.message);
+          setTimeout(changeToggleToFalse, 5000);
+        }
+      });
+    e.preventDefault();
+  };
 
-    return (
-        <div>
-            {toggle ? (
-                <h2>
-                    {response?.name} {response?.count} {error}
-                </h2>
-            ) : null}
-            <Input
-                text={"NAME"}
-                value={name}
-                id={"itemNameForPurchase"}
-                onChange={inputOnchangeHandler}
-            />
-            <Input
-                text={"DATE"}
-                value={count}
-                id={"date"}
-                onChange={inputOnchangeHandler}
-            />
-            <Button text={"AddPurchase"} onclick={addItemBtnHandler} />
-        </div>
-    );
+  return (
+    <div>
+      {toggle ? (
+        <h2>
+          {response?.name} {response?.price} {error}
+        </h2>
+      ) : null}
+      <Input
+        text={"NAME"}
+        value={name}
+        id={"itemNameForPurchase"}
+        onChange={inputOnchangeHandler}
+      />
+      <Input
+        text={"DATE"}
+        value={date}
+        id={"date"}
+        onChange={inputOnchangeHandler}
+      />
+      <Button text={"AddPurchase"} onclick={addItemBtnHandler} />
+    </div>
+  );
 };
 
 export default AddPurchase;
--- server/controllers/purchasesController/purchases.controller.js
+++ server/controllers/purchasesController/purchases.controller.js
@@ -1,37 +1,44 @@
 const text = require("../../const/text");
 const {
-    getCategoryRecordFromDB,
-    savePurchaseRecordToDB,
-    updateSnackCountInCategory,
-    getCategoryPriceByName,
+  getCategoryRecordFromDB,
+  savePurchaseRecordToDB,
+  updateSnackCountInCategory,
+  getCategoryPriceByName,
 } = require("../../services/purchase.service");
 
 class PurchasesController {
-    async createPurchase(req, res)  {
+  async createPurchase(req, res) {
     try {
-    const item = await getCategoryPriceByName({ name: req.body?.name });
-    const itemToSave = {
+      const item = await getCategoryPriceByName({ name: req.body?.name });
+      console.log(item);
+      if (!item) {
+        throw new Error("No item for purchase");
+      }
+      const itemToSave = {
         name: req.body?.name,
         date: req.body?.date,
-        price: item.price,
-    };
-    const snackData = await getCategoryRecordFromDB({
+        price: item?.price,
+      };
+      const snackData = await getCategoryRecordFromDB({
         name: `${req.body?.name}`,
-    });
-    if (snackData?.count > 0) {
-    await updateSnackCountInCategory(
-        { name: `${req.body?.name}` },
-{ count: snackData?.count - 1 }
-);
-const purchase = await savePurchaseRecordToDB(itemToSave);
-res.json(purchase);
-} else if (snackData?.count === 0) {
-res.json({ name: text.NO_ITEMS });
-}
-} catch (e) {
-console.log(e);
-}
-};
+      });
+      if (snackData?.count > 0) {
+        await updateSnackCountInCategory(
+          { name: `${req.body?.name}` },
+          { count: snackData?.count - 1 }
+        );
+        const purchase = await savePurchaseRecordToDB(itemToSave);
+        res.json(purchase);
+      } else if (snackData?.count === 0) {
+        res.json({ name: text.NO_ITEMS });
+      }
+    } catch (e) {
+      res.json({
+        message: e.message,
+        statusCode: 404,
+      });
+    }
+  }
 }
 
 module.exports = new PurchasesController();
